65dee6082a45ab4f4121e85ddb71b1b198bbd4ad
hrithikvaishnav <201851050@iiitvadodara.ac.in>
2021-08-29 22:39:19 +0530

    fix: minor changes

--- index.js
+++ index.js
@@ -14,11 +14,7 @@ require("./models/Expense");
 const requireAuth = require('./middleware/requireAuth');
 
 const app = express();
-app.use(
-    cors({
-        origin: "http://localhost:3000",
-    })
-)
+app.use(cors())
 app.use(bodyParser.json());
 
 const URI = process.env.URI;
